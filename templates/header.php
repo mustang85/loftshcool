a9f66f20a87dbd6413c09f15b827e53f6f1754ed
mustang85 <sergeant-kir@mail.ru>
2014-11-17 19:08:59 +0300

    changes header.php

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,14 @@
 <header>
-	<header>
-		<div class="share">
-			<a class="share__link vk" href="https://vk.com/id275779973">vkontakte</a>
-			<a class="share__link twitter" href="https://twitter.com/sergeant_kir">twitter</a>
-			<a class="share__link facebook" href="https://www.facebook.com/kirill.granev.5">facebook</a>
-			<a class="share__link github" href="https://github.com/mustang85">github</a>
-		</div>
-		<div class="logo">
-			<a class="logo__link" href="/index.html">
-				<img class="logo_img" src="img/logo.png" height="58" width="245" alt="" />
-			</a>
-		</div>
-	</header>
+	<div class="share">
+		<a class="share__link vk" href="https://vk.com/id275779973">vkontakte</a>
+		<a class="share__link twitter" href="https://twitter.com/sergeant_kir">twitter</a>
+		<a class="share__link facebook" href="https://www.facebook.com/kirill.granev.5">facebook</a>
+		<a class="share__link github" href="https://github.com/mustang85">github</a>
+	</div>
+	<div class="logo">
+		<a class="logo__link" href="/index.html">
+			<img class="logo_img" src="img/logo.png" height="58" width="245" alt="" />
+		</a>
+	</div>
 </header>
+
