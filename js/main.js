5cb2efa2e3ec234067b10e4e602a0fc6a390df1e
mustang85 <sergeant-kir@mail.ru>
2014-11-20 12:06:19 +0300

    add style and add script

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
+	/* animation contacts */
+
+	$('.contacts .list__link').hover(function() {
+		var currentIcon = $(this).find('.icon');
+		$(currentIcon).addClass('shake animated');
+	}, function() {
+		var currentIcon = $(this).find('.icon');
+		$(currentIcon).removeClass('animated shake')
+	});
+
 	/* validation form */
 	console.log('dsf');
 
